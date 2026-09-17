20efbad45294feb5396a84b1858ce722a6bf43b8
Niklas Kremser <n.kremser@tu-bs.de>
2026-09-17 11:55:52 +0200

    add Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
+function LoadAlarmButton({ title, href }: { title: string; href: string }) {
+  return (
+    <Link href={href}>
+      <button type="button">{title}</button>
+    </Link>
+  );
+}
+
 export default function Page() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <LoadAlarmButton title="Alarmtafel" href="/alarmtafel" />
+    </div>
+  );
 }
