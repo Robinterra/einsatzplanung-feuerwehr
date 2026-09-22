708609368eb025b7ca57b485f30aa00dd30d413f
Lisa Schulte <lisa.schulte@tu-braunschweig.de>
2026-09-22 10:19:49 +0200

    Einfaches Szenario

--- lib/controlling/controlScript.ts
+++ lib/controlling/controlScript.ts
@@ -1,4 +1,11 @@
 //hier können später verschiedene controlFunctions auf gerufen werden, um ein Szenario zu steuern
 
-async function newSzenarioWith40Members(){
+import { deleteAssignment, MembersArrive } from "./controlFunctions";
+
+async function newSzenarioWith40Members() {
+  await deleteAssignment();
+  for (let i = 0; i < 4; i++) {
+    await MembersArrive(10, null);
+    await new Promise((resolve) => setTimeout(resolve, 10_000));
+  }
 }
